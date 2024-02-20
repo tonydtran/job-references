e66f14678bf62a5dab25adf4903029f980cf884a
Anthony Tran Van <anthony.tranvan@gmail.com>
2024-02-20 20:36:23 +0100

    Use public link for LinkedIn

--- src/references.js
+++ src/references.js
@@ -11,7 +11,7 @@ export default [
     avatar: fatiCherkaoui,
     jobTitle: 'Product Manager',
     text: "<p>C'est un plaisir de recommander Anthony, avec qui j'ai travaillé pendant deux ans. En tant que développeur front-end, il assure techniquement et apporte une dimension appréciable avec son sens du design et du produit.</p><p>Anthony ne se contente pas seulement de coder efficacement, mais il enrichit également l'expérience de travail en apportant une perspective créative. Son expertise polyvalente en fait un atout indéniable pour toute équipe technique.</p><p>Dans notre collaboration, il a été une source constante d'idées innovantes, contribuant ainsi de manière significative à la qualité et à la convivialité de nos projets et je suis convaincu qu'il continuera à exceller dans ses futurs projets.</p>",
-    link: 'https://www.linkedin.com/in/anthonytranvan/details/recommendations/',
+    link: 'https://www.linkedin.com/in/anthonytranvan',
     source: 'LinkedIn',
   },
   {
@@ -19,7 +19,7 @@ export default [
     avatar: sixtineCaspar,
     jobTitle: 'Back End Developer',
     text: "<p>J’ai eu la chance de travailler avec Anthony pendant près de 2 ans sur de nombreux projets en paire front/back.</p><p>Anthony est efficace et toujours prêt à remettre les habitudes en questions, pour proposer des solutions innovantes et éclairées des dernières évolutions en termes de bonnes pratiques.</p><p>Il a une expertise approfondie en matière de développement front-end, avec le goût pour un code bien construit et efficace. De plus, il a une vraie appétence produit et expérience utilisateur et n’hésite pas à s’impliquer dans toutes les réflexions en amont du code.</p><p>Au-delà de ses compétences techniques, Anthony est une personne fiable et collaborative. Il est toujours prêt à partager ses connaissances avec les autres membres de l'équipe, sait expliquer des concepts complexes de manière claire et concise, et aime travailler ensemble pour atteindre nos objectifs communs.</p><p>En résumé, je recommande vivement Anthony en tant que développeur front-end talentueux et dédié. Je regrette déjà le temps de notre collaboration</p>",
-    link: 'https://www.linkedin.com/in/anthonytranvan/details/recommendations/',
+    link: 'https://www.linkedin.com/in/anthonytranvan',
     source: 'LinkedIn',
   },
   {
@@ -43,7 +43,7 @@ export default [
     avatar: nadiaBourai,
     jobTitle: 'Software QA Engineer',
     text: '<p>Travailler avec Anthony est expérience exceptionnelle. Son dévouement, et son attitude positive ont grandement contribué à la réussite de nos projets communs. Un professionnel très efficace et une personne agréable à avoir dans une équipe.</p>',
-    link: 'https://www.linkedin.com/in/anthonytranvan/details/recommendations/',
+    link: 'https://www.linkedin.com/in/anthonytranvan',
     source: 'LinkedIn',
   },
   {
@@ -51,7 +51,7 @@ export default [
     avatar: pierreFilstroff,
     jobTitle: 'Back End Developer, Android/iOS Developer, DevOPS',
     text: "<p>Anthony est un développeur efficace et pragmatique. Il est méticuleux et s'adapte à la moindre tâche qui lui est soumise. Curieux et constamment à la recherche de l'élargissement de son scope de compétence, il est aussi et surtout une personne avec qui l'ont peut facilement communiquer, à l'écoute et bien-veillante. J'ai eu un réel plaisir à travailler avec lui et lui souhaite bien évidemment le meilleur pour son avenir professionnel.</p>",
-    link: 'https://www.linkedin.com/in/anthonytranvan/details/recommendations/',
+    link: 'https://www.linkedin.com/in/anthonytranvan',
     source: 'LinkedIn',
   },
 ]
